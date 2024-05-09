e0d2a82778ec4fc4352fc520ed4898514e457ab3
leo <leopoldo.miranda18@tectijuana.edu.mx>
2024-05-09 00:09:51 -0700

    ✨ add Benefits component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
+import Benefits from "./components/Benefits.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <Benefits />
 
   <div
     id="faq"
